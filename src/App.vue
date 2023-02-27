9552697c6cf3a244937be920e1132e1c15a79d6c
Artur <64634525+asarnacki@users.noreply.github.com>
2023-02-27 11:08:46 +0100

    #2 rendering data (#3)
    * added navigation component
    
    * added colors to tailwind
    
    ---------
    
    Co-authored-by: Artur Sarnacki <Artur.Sarnacki@komputronik.pl>

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1 class="mt-8 text-center text-6xl">Vue test</h1>
+  <!-- <MainNav /> -->
+  <main-nav />
 </template>
+
+<script>
+import MainNav from "@/components/MainNav.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainNav,
+  },
+};
+</script>
