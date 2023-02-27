6132043ef20a0ca507a8657d8e787f00bf98df32
Artur Sarnacki <Artur.Sarnacki@komputronik.pl>
2023-02-27 11:08:06 +0100

    added colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        "brand-gray-1": "#dadce0",
+        "brand-blue-1": "#1967d2",
+        "brand-green-1": "#137333",
+        /* added custom color to tailwind, now its possible to create own classes with this color
+         ex. sample class - text-black,
+         custom color class text-brand-gray-1 */
+      },
     },
   },
   plugins: [],
