9ae14b778e7794c24c0d1d4e4285eea7bb37f4d9
Artur <64634525+asarnacki@users.noreply.github.com>
2023-03-13 08:33:30 +0100

    #4 directives (#5)
    * added some tests and data to component
    
    * added and styled button component
    
    * added profile image component
    
    * test profile image and button component
    
    * bump priettier
    
    ---------
    
    Co-authored-by: Artur Sarnacki <Artur.Sarnacki@komputronik.pl>

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,19 +4,53 @@
       <div
         class="flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
       >
-        <a href="/" class="flex h-full items-center text-xl">{{ appName }}</a>
+        <a :href="url" class="flex h-full items-center text-xl">{{
+          appName
+        }}</a>
+
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li
+              v-for="item in menuItems"
+              :key="item"
+              class="ml-9 h-full first:ml-0"
+            >
+              <a href="" class="flex h-full items-center py-2.5">{{ item }}</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div class="ml-auto flex h-full items-center">
+          <profile-image v-if="isLoggedIn" />
+          <action-button v-else @click="loginUser" />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from "@/components/ActionButton.vue";
+import ProfileImage from "@/components/ProfileImage.vue";
+
 export default {
   name: "MainNav",
+  components: {
+    ActionButton,
+    ProfileImage,
+  },
   data() {
     return {
       appName: "G-Searcher",
+      url: "https://dolmar.pl/",
+      menuItems: ["Home", "Life at company", "Shop", "Careers", "Contact"],
+      isLoggedIn: false,
     };
   },
+  methods: {
+    loginUser() {
+      this.isLoggedIn = true;
+    }
+  }
 };
 </script>
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
 
 import MainNav from "@/components/MainNav.vue";
 
@@ -7,8 +8,45 @@ describe("MainNav", () => {
     render(MainNav);
     screen.debug();
     const appName = screen.getByText("G-Searcher");
+    const navItem = screen.getByText("Contact");
     expect(appName).toBeInTheDocument();
+    expect(navItem).toBeInTheDocument();
+  });
+
+  it("displays menu items in nav", () => {
+    render(MainNav);
+    const navMenuItems = screen.getAllByRole("listitem");
+    const navMenuTexts = navMenuItems.map((item) => item.textContent);
+    console.log(navMenuTexts);
+
+    expect(navMenuTexts).toEqual([
+      "Home",
+      "Life at company",
+      "Shop",
+      "Careers",
+      "Contact",
+    ]);
+
+  });
+
+  describe("when user log in", () => {
+    it("displays user profile pic", async () => {
+      render(MainNav);
+
+      let profileImage = screen.queryByRole("img", {
+        name: /user profile image/i,
+      });
+      expect(profileImage).not.toBeInTheDocument();
+
+      const loginButton = screen.queryByRole("button", {
+        name: /sign in/i,
+      });
+      await userEvent.click(loginButton);
+
+      profileImage = screen.getByRole("img", {
+        name: /user profile image/i,
+      });
+      expect(profileImage).toBeInTheDocument();
+    });
   });
 });
-
-
