80b1fd0119f82c7ea6719b560aa5cb6f02637fc7
Artur Sarnacki <Artur.Sarnacki@komputronik.pl>
2023-03-20 12:09:58 +0100

    added tests to SubNav

---
+++ tests/unit/components/SubNav.test.js
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/vue";
+import SubNav from "@/components/SubNav.vue";
+
+describe("SubNav", () => {
+  describe("when user is on jobs page", () => {
+    it("displays job count", () => {
+      render(SubNav, {
+        global: {
+          stubs: {
+            FontAwesoneIcon: true,
+          },
+        },
+        data() {
+          return { onJobResultsPage: true };
+        },
+      });
+      const jobCount = screen.getByText("1655");
+      expect(jobCount).toBeInTheDocument();
+    });
+  });
+  describe("when user is on jobs page", () => {
+    it("does NOT display job count", () => {
+      render(SubNav, {
+        global: {
+          stubs: {
+            FontAwesoneIcon: true,
+          },
+        },
+        data() {
+          return { onJobResultsPage: false };
+        },
+      });
+      const jobCount = screen.queryByText("1655");
+      expect(jobCount).not.toBeInTheDocument();
+    });
+  });
+});
