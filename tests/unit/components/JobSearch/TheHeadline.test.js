9cb75687f3e87ae51064a4eb4fb357d387a0a7eb
Artur Sarnacki <Artur.Sarnacki@komputronik.pl>
2023-04-11 12:08:26 +0200

    isolating components, refactor

--- tests/unit/components/JobSearch/TheHeadline.test.js
+++ tests/unit/components/JobSearch/TheHeadline.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/vue";
-import TheHeadline from "@/components/TheHeadline.vue";
+import TheHeadline from "@/components/JobSearch/TheHeadline.vue";
 import { vi } from "vitest";
 import { nextTick } from "vue";
 
