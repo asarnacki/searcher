cbfbd019bcb5f589a33c1cb6d80fab9034200e0f
Artur Sarnacki <Artur.Sarnacki@komputronik.pl>
2023-04-27 11:24:14 +0200

    separate TextInput component

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -7,12 +7,7 @@
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
         <label class="absolute left-0 -top-10">Role</label>
-        <input
-          v-model="role"
-          type="text"
-          placeholder="Software engineer"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input v-model="role" placeholder="Software engineer" />
       </div>
 
       <span
@@ -23,12 +18,7 @@
 
       <div class="relative flex h-full flex-1 items-center pl-3">
         <label class="absolute left-0 -top-10">Where?</label>
-        <input
-          v-model="location"
-          type="text"
-          placeholder="Warsaw"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input v-model="location" placeholder="Warsaw" />
       </div>
     </div>
 
@@ -38,9 +28,10 @@
 
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
+import TextInput from "@/components/Shared/TextInput.vue";
 export default {
   name: "JobSearchForm",
-  components: { ActionButton },
+  components: { ActionButton, TextInput },
   data() {
     return {
       role: "",
---
+++ tests/unit/components/Shared/TextInput.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
+import TextInput from "@/components/Shared/TextInput.vue";
+describe("TextInput", () => {
+  it("coommunicates that user has entered character", async () => {
+    const { emitted } = render(TextInput, {
+      props: {
+        modelValue: "",
+      },
+    });
+    const input = screen.getByRole("textbox");
+    await userEvent.type(input, "NYC");
+    const messages = emitted()["update:modelValue"];
+    expect(messages).toEqual([["N"], ["NY"], ["NYC"]]);
+  });
+});
