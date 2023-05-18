2a2ba20ed02bd93488edc76e049aac377332ee1b
Artur <64634525+asarnacki@users.noreply.github.com>
2023-05-18 10:12:20 +0200

    #12 create backend (#13)
    * configured backend, installed axios
    
    * change hardcoded data to backend logic
    
    * added  JobListing(s) tests
    
    * small changes
    
    ---------
    
    Co-authored-by: Artur Sarnacki <Artur.Sarnacki@komputronik.pl>

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,21 +1,29 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
     >
       <div class="mx-8 border-b border-solid border-brand-gray-2 pt-5 pb-2">
         <h2 class="mb-2 text-2xl">
-          Technical Program Menager, Perception, Augmented Reality
+          {{ job.title }}
         </h2>
 
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Sarna</span>
+            <span>{{ job.organization }}</span>
           </div>
 
           <div>
-            <span>Warsaw, PL</span>
+            <ul>
+              <li
+                v-for="location in job.locations"
+                :key="location"
+                class="mr-5 inline-block"
+              >
+                <span>{{ location }}</span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -25,18 +33,17 @@
           <h3 class="mt-1 mb-2">Qualifications:</h3>
           <div>
             <ul class="list-disc pl-8">
-              <li>Bachelor's degree or equivalent practical experience.</li>
-              <li>
-                2 years of experience with software development in one or more
-                programming languages, or 1 year of experience with an advanced
-                degree.
+              <li
+                v-for="qualification in job.minimumQualifications"
+                :key="qualification"
+              >
+                <span>{{ qualification }}</span>
               </li>
-              <li>2 years of experience with data structures or algorithms.</li>
             </ul>
           </div>
         </div>
         <div class="mt-2 text-center">
-          <router-link to="/jobs/results/1" class="text-brand-blue-1"
+          <router-link :to="jobPageLink" class="text-brand-blue-1"
             >Expand</router-link
           >
         </div>
@@ -48,5 +55,16 @@
 <script>
 export default {
   name: "JobListing",
+  props: {
+    job: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    jobPageLink() {
+      return `/jobs/results/${this.job.id}`;
+    },
+  },
 };
 </script>
