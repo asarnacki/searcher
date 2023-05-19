66d05a8b9b98b84a5891d1cb7b7efa3c69db6262
Artur <64634525+asarnacki@users.noreply.github.com>
2023-05-19 06:06:40 +0200

    #13 dynamic pagination (#14)
    * added dynamic pagination
    
    * added tests to dynamic pagination
    
    ---------
    
    Co-authored-by: Artur Sarnacki <Artur.Sarnacki@komputronik.pl>

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,7 +1,32 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <job-listing v-for="job in jobs" :key="job.id" :job="job" />
+      <job-listing v-for="job in displayedJobs" :key="job.id" :job="job" />
+
+      <div class="mx-auto mt-8">
+        <div class="flex flex-row flex-nowrap">
+          <p class="flex-grow text-sm">Page {{ currentPage }}</p>
+
+          <div class="flex items-center justify-center">
+            <router-link
+              v-if="previousPage"
+              class="mx-3 text-sm font-semibold text-brand-blue-1"
+              :to="{ name: 'JobResults', query: { page: previousPage } }"
+              role="link"
+            >
+              Previous
+            </router-link>
+            <router-link
+              v-if="nextPage"
+              class="mx-3 text-sm font-semibold text-brand-blue-1"
+              :to="{ name: 'JobResults', query: { page: nextPage } }"
+              role="link"
+            >
+              Next
+            </router-link>
+          </div>
+        </div>
+      </div>
     </ol>
   </main>
 </template>
@@ -17,8 +42,32 @@ export default {
       jobs: [],
     };
   },
+  computed: {
+    displayedJobs() {
+      const firstJobIndex = (this.currentPage - 1) * 10;
+      const lastJobIndex = this.currentPage * 10;
+      return this.jobs.slice(firstJobIndex, lastJobIndex);
+    },
+
+    previousPage() {
+      const previousPage = this.currentPage - 1;
+      const firstPage = 1;
+      return previousPage >= firstPage ? previousPage : undefined;
+    },
+
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      const lastPage = Math.ceil(this.jobs.length / 10);
+      return nextPage <= lastPage ? nextPage : undefined;
+    },
+
+    currentPage() {
+      return Number.parseInt(this.$route.query.page || "1");
+    },
+  },
   async mounted() {
-    const response = await axios.get("http://localhost:3000/jobs");
+    const baseUrl = import.meta.env.VITE_APP_API_URL;
+    const response = await axios.get(`${baseUrl}/jobs`);
     this.jobs = response.data;
   },
 };
