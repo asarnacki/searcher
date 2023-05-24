dab44d1a74fc914a8ae14edef48b44378fe9aef9
Artur Sarnacki <Artur.Sarnacki@komputronik.pl>
2023-05-24 06:52:49 +0200

    refactor

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -32,43 +32,44 @@
 </template>
 
 <script>
-import axios from "axios";
+import { mapActions, mapState } from "pinia";
 import JobListing from "@/components/JobResults/JobListing.vue";
+import { useJobsStore, FETCH_JOBS } from "@/stores/jobs";
+
 export default {
   name: "JobListings",
   components: { JobListing },
-  data() {
-    return {
-      jobs: [],
-    };
-  },
   computed: {
-    displayedJobs() {
-      const firstJobIndex = (this.currentPage - 1) * 10;
-      const lastJobIndex = this.currentPage * 10;
-      return this.jobs.slice(firstJobIndex, lastJobIndex);
-    },
-
     previousPage() {
       const previousPage = this.currentPage - 1;
       const firstPage = 1;
       return previousPage >= firstPage ? previousPage : undefined;
     },
 
-    nextPage() {
-      const nextPage = this.currentPage + 1;
-      const lastPage = Math.ceil(this.jobs.length / 10);
-      return nextPage <= lastPage ? nextPage : undefined;
-    },
+    ...mapState(useJobsStore, {
+      jobs: "jobs",
+      displayedJobs() {
+        const firstJobIndex = (this.currentPage - 1) * 10;
+        const lastJobIndex = this.currentPage * 10;
+        return this.jobs.slice(firstJobIndex, lastJobIndex);
+      },
+      nextPage() {
+        const nextPage = this.currentPage + 1;
+        const lastPage = Math.ceil(this.jobs.length / 10);
+        return nextPage <= lastPage ? nextPage : undefined;
+      },
+    }),
 
     currentPage() {
       return Number.parseInt(this.$route.query.page || "1");
     },
   },
   async mounted() {
-    const baseUrl = import.meta.env.VITE_APP_API_URL;
-    const response = await axios.get(`${baseUrl}/jobs`);
-    this.jobs = response.data;
+    this.FETCH_JOBS();
+  },
+
+  methods: {
+    ...mapActions(useJobsStore, [FETCH_JOBS]),
   },
 };
 </script>
