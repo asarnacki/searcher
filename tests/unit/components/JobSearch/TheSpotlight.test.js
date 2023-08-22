a8925dfffa8ea9c86c40a179c67ae7d5dbe71ce1
Artur <64634525+asarnacki@users.noreply.github.com>
2023-08-22 12:45:07 +0200

    #15 slots (#19)
    * added and extract component
    
    * refactor
    
    * added CollapsibleAccordion compoenent and tests
    
    * added font awesome caret icons
    
    * added named slots and tests
    
    * added TheSpotlight component
    
    * updated db
    
    ---------
    
    Co-authored-by: Artur Sarnacki <Artur.Sarnacki@komputronik.pl>

---
+++ tests/unit/components/JobSearch/TheSpotlight.test.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+import TheSpotlight from "@/components/JobSearch/TheSpotlight.vue";
+import { render, screen } from "@testing-library/vue";
+
+vi.mock("axios");
+
+describe("TheSpotlight", () => {
+  const mockSpotlightResponse = (spotlight = {}) => {
+    axios.get.mockResolvedValue({
+      data: [
+        {
+          id: 1,
+          img: "Some img",
+          title: "Some title",
+          description: "Some description",
+          ...spotlight,
+        },
+      ],
+    });
+  };
+
+  it("Provides img to parent component", async () => {
+    const spotlight = { img: "Other img" };
+    mockSpotlightResponse(spotlight);
+
+    render(TheSpotlight, {
+      slots: {
+        default: `<template #default="slotProps">
+          <h1>{{ slotProps.img }}</h1>
+        </template>`,
+      },
+    });
+
+    const text = await screen.findByText("Other img");
+    expect(text).toBeInTheDocument();
+  });
+
+  it("Provides title to parent component", async () => {
+    const spotlight = { title: "Other title" };
+    mockSpotlightResponse(spotlight);
+
+    render(TheSpotlight, {
+      slots: {
+        default: `<template #default="slotProps">
+          <h1>{{ slotProps.title }}</h1>
+        </template>`,
+      },
+    });
+
+    const text = await screen.findByText("Other title");
+    expect(text).toBeInTheDocument();
+  });
+
+  it("Provides description to parent component", async () => {
+    const spotlight = { description: "Other description" };
+    mockSpotlightResponse(spotlight);
+
+    render(TheSpotlight, {
+      slots: {
+        default: `<template #default="slotProps">
+          <h1>{{ slotProps.description }}</h1>
+        </template>`,
+      },
+    });
+
+    const text = await screen.findByText("Other description");
+    expect(text).toBeInTheDocument();
+  });
+});
