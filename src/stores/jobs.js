8cb3b64772078b16fb79e21b84d004c48824e6b0
Artur Sarnacki <Artur.Sarnacki@komputronik.pl>
2023-10-04 12:01:23 +0200

    added filtering jobs by organizations

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -1,8 +1,10 @@
 import { defineStore } from "pinia";
+import { useUserStore } from "@/stores/user";
 import getJobs from "@/api/getJobs";
 
 export const FETCH_JOBS = "FETCH_JOBS";
 export const UNIQUE_ORGANIZATIONS = "UNIQUE_ORGANIZATIONS";
+export const FILTERED_JOBS_BY_ORGANIZATIONS = "FILTERED_JOBS_BY_ORGANIZATIONS";
 
 export const useJobsStore = defineStore("jobs", {
   state: () => ({
@@ -23,6 +25,17 @@ export const useJobsStore = defineStore("jobs", {
         uniqueOrganiztions.add(job.organization);
       });
       return uniqueOrganiztions;
+    },
+
+    [FILTERED_JOBS_BY_ORGANIZATIONS](state) {
+      const userStore = useUserStore();
+
+      if (userStore.selectedOrganizations.length === 0) {
+        return state.jobs;
+      }
+      return state.jobs.filter((job) =>
+        userStore.selectedOrganizations.includes(job.organization)
+      );
     },
   },
 });
