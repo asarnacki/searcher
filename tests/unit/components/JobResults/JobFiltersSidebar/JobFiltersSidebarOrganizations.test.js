aecfa4619c957e35c1dc25816ce3ca625b130f8d
Artur Sarnacki <Artur.Sarnacki@komputronik.pl>
2023-10-04 12:01:41 +0200

    added tests to filtering jobs by organizations

--- tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.test.js
+++ tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.test.js
@@ -4,12 +4,13 @@ import { createTestingPinia } from "@pinia/testing";
 
 import JobFiltersOrganizations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue";
 import { useJobsStore } from "@/stores/jobs";
+import { useUserStore } from "@/stores/user";
 
 describe("JobFiltersOrganizations", () => {
-  it("renders unique list of organizations from jobs", async () => {
+  const renderJobFilterSidebarOrganizations = () => {
     const pinia = createTestingPinia();
     const jobsStore = useJobsStore();
-    jobsStore.UNIQUE_ORGANIZATIONS = new Set(["Google", "Amazon"]);
+    const userStore = useUserStore();
 
     render(JobFiltersOrganizations, {
       global: {
@@ -19,11 +20,34 @@ describe("JobFiltersOrganizations", () => {
         },
       },
     });
+
+    return { jobsStore, userStore };
+  };
+
+  it("renders unique list of organizations from jobs", async () => {
+    const { jobsStore } = renderJobFilterSidebarOrganizations();
+    jobsStore.UNIQUE_ORGANIZATIONS = new Set(["Google", "Amazon"]);
+
     const button = screen.getByRole("button", { name: /organizations/i });
     await userEvent.click(button);
 
     const organizationListItems = screen.getAllByRole("listitem");
     const organizations = organizationListItems.map((node) => node.textContent);
     expect(organizations).toEqual(["Google", "Amazon"]);
+  });
+
+  it("communicates that user selected checkbox for organization", async () => {
+    const { jobsStore, userStore } = renderJobFilterSidebarOrganizations();
+    jobsStore.UNIQUE_ORGANIZATIONS = new Set(["Google", "Amazon"]);
+
+    const button = screen.getByRole("button", { name: /organizations/i });
+    await userEvent.click(button);
+
+    const googleCheckbox = screen.getByRole("checkbox", { name: /google/i });
+    await userEvent.click(googleCheckbox);
+
+    expect(userStore.ADD_SELECTED_ORGANIZATIONS).toHaveBeenCalledWith([
+      "Google",
+    ]);
   });
 });
--- tests/unit/stores/jobs.test.js
+++ tests/unit/stores/jobs.test.js
@@ -1,5 +1,6 @@
 import { createPinia, setActivePinia } from "pinia";
 import { useJobsStore } from "@/stores/jobs";
+import { useUserStore } from "@/stores/user";
 import axios from "axios";
 
 vi.mock("axios");
@@ -49,6 +50,47 @@ describe("getteres", () => {
       const result = store.UNIQUE_ORGANIZATIONS;
 
       expect(result).toEqual(new Set(["Google", "Amazon"]));
+    });
+  });
+
+  describe("FILTERED_JOBS_ORGANIZATIONS", () => {
+    it("identifies jobs that are associated with given organizations", () => {
+      const jobsStore = useJobsStore();
+      jobsStore.jobs = [
+        { organization: "Google" },
+        { organization: "Amazon" },
+        { organization: "Microsoft" },
+      ];
+
+      const userStore = useUserStore();
+      userStore.selectedOrganizations = ["Google", "Microsoft"];
+
+      const result = jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS;
+      expect(result).toEqual([
+        { organization: "Google" },
+        { organization: "Microsoft" },
+      ]);
+    });
+  });
+
+  describe("when user has not selected organizations", () => {
+    it("return all jobs", () => {
+      const jobsStore = useJobsStore();
+      jobsStore.jobs = [
+        { organization: "Google" },
+        { organization: "Amazon" },
+        { organization: "Microsoft" },
+      ];
+
+      const userStore = useUserStore();
+      userStore.selectedOrganizations = [];
+      const result = jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS;
+
+      expect(result).toEqual([
+        { organization: "Google" },
+        { organization: "Amazon" },
+        { organization: "Microsoft" },
+      ]);
     });
   });
 });
