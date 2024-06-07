799e5aa2fd1c46e5ad15e9df428a8a78b8c07216
Artur Sarnacki <Artur.Sarnacki@komputronik.pl>
2024-06-07 12:48:35 +0200

    fixed wrong variables

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue
@@ -1,18 +1,18 @@
 <template>
   <job-filters-sidebar-checkbox-group
     :unique-values="UNIQUE_DEGREES"
-    :action="userStore.ADD_SELECTED_DEGREES"
+    :action="userStore.ADD_SELECTED_JOB_TYPES"
   />
 </template>
 
 <script lang="ts" setup>
 import { computed } from "vue";
 import JobFiltersSidebarCheckboxGroup from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue";
-import { useDegreesStore } from "@/stores/degrees";
+import { useJobsStore } from "@/stores/jobs";
 import { useUserStore } from "@/stores/user";
 
 const userStore = useUserStore();
-const degreesStore = useDegreesStore();
+const jobsStore = useJobsStore();
 
-const UNIQUE_DEGREES = computed(() => degreesStore.UNIQUE_DEGREES);
+const UNIQUE_DEGREES = computed(() => jobsStore.UNIQUE_JOB_TYPES);
 </script>
