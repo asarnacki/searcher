3b4a90d42445b9d9a990f3b4fba941f7a86f6672
Artur Sarnacki <Artur.Sarnacki@komputronik.pl>
2024-06-07 12:38:14 +0200

    extract JobsFilterSidebar into smaller components

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarDegrees.vue
@@ -0,0 +1,18 @@
+<template>
+  <job-filters-sidebar-checkbox-group
+    :unique-values="UNIQUE_JOB_TYPES"
+    :action="userStore.ADD_SELECTED_DEGREES"
+  />
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import JobFiltersSidebarCheckboxGroup from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue";
+import { useDegreesStore } from "@/stores/degrees";
+import { useUserStore } from "@/stores/user";
+
+const userStore = useUserStore();
+const degreesStore = useDegreesStore();
+
+const UNIQUE_JOB_TYPES = computed(() => degreesStore.UNIQUE_DEGREES);
+</script>
