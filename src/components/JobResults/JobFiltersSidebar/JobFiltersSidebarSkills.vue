d5081d078ea02c3e77d10c821a776e44451b0784
Artur <sarnackiartur@gmail.com>
2024-06-09 13:54:40 +0200

    added JobFilterSidebar input

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mt-2">
+    <input
+      v-model.lazy.trim="skillSearchTerm"
+      type="text"
+      class="border-color-brand-gray-1 h-12 w-full rounded border-solid p-3 text-base shadow-gray"
+      placeholder="Computer programming, Finance degree"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useUserStore } from "@/stores/user";
+import { computed } from "vue";
+
+const userStore = useUserStore();
+
+const skillSearchTerm = computed({
+  get() {
+    return userStore.skillsSearchTerm;
+  },
+  set(value: string) {
+    userStore.UPDATE_SKILLS_SEARCH_TERM(value);
+  },
+});
+</script>
