139f520e26f3a4b1370094703eaaf28d61ff4842
Artur <sarnackiartur@gmail.com>
2025-02-10 15:24:54 +0100

    changed services and components for JSONBin API

--- src/api/getDegrees.ts
+++ src/api/getDegrees.ts
@@ -1,11 +1,11 @@
 import apiClient from "@/api/axiosConfig";
 
-import type { Degree } from "@/api/types";
+import type { ApiResponse } from "@/api/types";
 
 const getDegrees = async () => {
-  console.log(import.meta.env.VITE_APP_API_ACCESS_KEY);
-  const response = await apiClient.get<Degree[]>("/");
-  return response.data;
+  const response = await apiClient.get<ApiResponse>("/");
+
+  return response.data.record.degrees;
 };
 
 export default getDegrees;
